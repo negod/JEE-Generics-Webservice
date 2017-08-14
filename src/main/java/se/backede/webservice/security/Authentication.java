32840bd211831eed0b193dd5e23594aea6102920
Joakim Backede <joakim.backede@outlook.com>
2017-08-14 19:08:24 +0200

    Made interface abstract

---
+++ src/main/java/se/backede/webservice/security/Authentication.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.backede.webservice.security;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import static javax.ws.rs.core.HttpHeaders.AUTHORIZATION;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import static javax.ws.rs.core.Response.Status.UNAUTHORIZED;
+import javax.ws.rs.core.UriInfo;
+
+/**
+ *
+ * @author Joakim Backede ( joakim.backede@outlook.com )
+ */
+@Stateless
+public abstract class Authentication {
+
+    abstract public AuthorizationDao getDao();
+
+    @Context
+    UriInfo uriInfo;
+
+    @EJB
+    KeyProvider keyProvider;
+
+    @POST
+    @Path("/login")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response authenticate(Credentials credentials) {
+        try {
+            if (getDao().authorize(credentials)) {
+                return Response.ok().header(AUTHORIZATION, keyProvider.generateJWT(credentials.getUsername(), uriInfo)).build();
+            }
+        } catch (AuthorizationException ex) {
+            return Response.status(UNAUTHORIZED).build();
+        }
+        return Response.status(UNAUTHORIZED).build();
+    }
+
+}
